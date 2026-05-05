3826dc791e8894cfcc3bce71a14b9cb6da6e93fa
unknown <sreesudhiravvaru@gmail.com>
2026-05-05 14:08:37 +0530

    feat: add industrial RTOS firmware skeleton

---
+++ setup.bat
@@ -0,0 +1,16 @@
+@echo off
+mkdir src\tasks
+mkdir include\rtos
+mkdir include\tasks
+mkdir include\drivers
+mkdir drivers\gpio
+mkdir drivers\uart
+mkdir drivers\spi
+mkdir drivers\i2c
+mkdir .github\workflows
+mkdir cmake
+mkdir docs\doxygen
+mkdir tests\unit
+mkdir tests\mocks
+mkdir config
+echo Folders created!
